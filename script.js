849fcbb8152a0ad61dfdfb52245005ae6c003575
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-04-13 23:36:28 +0500

    Update display when button is pressed

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const buttons = document.querySelectorAll(".numpad_button").forEach(button => {
+const buttons = document.querySelectorAll(".numpad_button");
+const display = document.getElementById("display");
+
+buttons.forEach(button => {
     const value = button.getAttribute("data-value");
     //display the pressed button in the console
     button.addEventListener("click", () => {
-        console.log("Button pressed:", value)
-    })
-})
+        console.log("Button pressed:", value);
+        // clear display when CE is pressed
+        if(value === "CE"){
+            display.innerText = ""
+        } else {
+            display.innerText += value;
+        }
+    });
+});
