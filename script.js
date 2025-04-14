cebf58af89084f6085bb545a21945a6d52028321
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-04-14 23:38:03 +0500

    Add operate function with 4 operations

--- script.js
+++ script.js
@@ -3,6 +3,55 @@ const display = document.getElementById("display");
 let dotPressed = false;
 const operators = ["+", "-", "/", "*"]
 let operatorPressed = false;
+let result;
+
+function add(a, b){
+    result = a + b;
+    console.log(`Result is: ${result}`)
+    return result;
+}
+function subtract(a, b){
+    result = a - b;
+    console.log(`Result is: ${result}`)
+    return result;
+}
+function multiply(a, b){
+    result = a * b;
+    console.log(`Result is: ${result}`)
+    return result;
+}
+function divide(a, b){
+    result = a / b;
+    console.log(`Result is: ${result}`)
+    return result;
+}
+
+function operate(){
+    const operatorMatch = display.innerText.match(/[+\-*/]/);
+  if (!operatorMatch) {
+    display.innerText = "Operator not found";
+    return;
+  }
+  const operator = operatorMatch[0];
+  console.log(`Operator is: ${operator}`)
+  let operands = display.innerText.split(/[+\-*/]/)
+  console.log(`Operands are: ${operands}`)
+  const firstOperand = parseFloat(operands[0])
+  const secondOperand = parseFloat(operands[1])
+  if(operator === "+"){
+    return add(firstOperand, secondOperand)
+  }
+  if(operator === "-"){
+    return subtract(firstOperand, secondOperand)
+  }
+  if(operator === "/"){
+    return divide(firstOperand, secondOperand)
+  }
+  if(operator === "*"){
+    return multiply(firstOperand, secondOperand)
+  }
+  
+}
 
 buttons.forEach(button => {
     const value = button.getAttribute("data-value");
@@ -27,9 +76,12 @@ buttons.forEach(button => {
                 dotPressed = false;
                 operatorPressed = true;
             } 
+        } else if(value === "=") {
+            operate()
+            display.innerText = result;
         } else {
             display.innerText += value;
             operatorPressed = false;
-        }
+        } 
     });
 });
