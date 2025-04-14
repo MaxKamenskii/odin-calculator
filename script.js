0fa37dbe8ed16025742e83dc05c9ecadb3e7561f
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-04-14 23:21:56 +0500

    Prevent entering  an operator twice

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const buttons = document.querySelectorAll(".numpad_button");
 const display = document.getElementById("display");
 let dotPressed = false;
+const operators = ["+", "-", "/", "*"]
+let operatorPressed = false;
 
 buttons.forEach(button => {
     const value = button.getAttribute("data-value");
     //display the pressed button in the console
     button.addEventListener("click", () => {
+        // track last character to prevert entering an operator twice
+        const lastChar = display.innerText.slice(-1);
+
         console.log("Button pressed:", value);
         // clear display when CE is pressed
         if(value === "CE"){
@@ -16,8 +21,15 @@ buttons.forEach(button => {
                 display.innerText += value;
                 dotPressed = true;
             }
+        } else if(operators.includes(value)) {
+            if(!operators.includes(lastChar)) {
+                display.innerText += value;
+                dotPressed = false;
+                operatorPressed = true;
+            } 
         } else {
             display.innerText += value;
+            operatorPressed = false;
         }
     });
 });
